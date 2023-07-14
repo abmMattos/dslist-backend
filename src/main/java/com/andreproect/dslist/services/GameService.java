94c13a9354523d57db63cfbade1a577e0827cdcd
André Barcelos Michels de Mattos <112213284+abmMattos@users.noreply.github.com>
2023-07-14 12:26:08 -0300

    search by list

--- src/main/java/com/andreproect/dslist/services/GameService.java
+++ src/main/java/com/andreproect/dslist/services/GameService.java
@@ -3,6 +3,7 @@ package com.andreproect.dslist.services;
 import com.andreproect.dslist.dto.GameDTO;
 import com.andreproect.dslist.entities.Game;
 import com.andreproect.dslist.dto.GameMinDTO;
+import com.andreproect.dslist.projections.GameMinProjection;
 import com.andreproect.dslist.repositories.GameRepository;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,6 +27,12 @@ public class GameService {
     @Transactional(readOnly = true)
     public List<GameMinDTO> findAll(){
         List<Game> result = gameRepository.findAll();
+        return result.stream().map(x -> new GameMinDTO(x)).toList();
+    }
+
+    @Transactional(readOnly = true)
+    public List<GameMinDTO> findByList(Long listId){
+        List<GameMinProjection> result = gameRepository.searchByList(listId);
         return result.stream().map(x -> new GameMinDTO(x)).toList();
     }
 }
